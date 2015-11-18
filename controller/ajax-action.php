fdecfbd2e1691a05cb37b2f25e06e98d18b3b09d
michaelannor <m.annor13@gmail.com>
2015-11-18 12:28:27 +0000

    cases in ajax-action done

--- controller/ajax-action.php
+++ controller/ajax-action.php
@@ -9,6 +9,35 @@
   $cmd = $_REQUEST['cmd'];
   switch ($cmd) {
     case 1:
+      cmd_get_all_orders();
+      break;
+
+    case 2:
+      get_order_by_id();
+      break;
+
+    case 3:
+      cmd_set_order_ready();
+      break;
+
+    case 4:
+      cmd_set_order_discharged();
+      break;
+
+    case 5:
+      cmd_get_all_meals();
+      break;
+
+    case 6:
+      cmd_get_meal_by_id();
+      break;
+
+    case 7:
+      cmd_set_meal_available();
+      break;
+
+    case 8:
+      cmd_set_meal_unavailable();
       break;
 
     default:
